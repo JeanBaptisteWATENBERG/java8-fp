a8ede1edb78d01f14c59bc9fa7cbc27221873c2f
Jean-Baptise WATENBERG <jeanbaptiste.watenberg@gmail.com>
2018-04-20 22:43:42 +0200

    Refactor to procedural

--- src/main/java/com/jb/fp/JavaFp.java
+++ src/main/java/com/jb/fp/JavaFp.java
@@ -10,8 +10,27 @@ import java.util.stream.Collectors;
  * @author jeanbaptiste.watenberg@gmail.com
  */
 public class JavaFp {
-
     public List<Movie> findByTitle(String title, List<Movie> movies) {
+        List<Movie> result = new ArrayList<>();
+        for (Movie movie: movies) {
+            addIfMatches(title, movie, result);
+        }
+        return result;
+    }
+
+    private void addIfMatches(String title, Movie movie, List<Movie> result) {
+        if (matches(title, movie)) result.add(movie);
+    }
+
+    private boolean matches(String title, Movie movie) {
+        return isInfixOf(movie.getTitle(), title);
+    }
+
+    private boolean isInfixOf(String whole, String title) {
+        return whole.contains(title);
+    }
+
+    public List<Movie> findByTitleBasic(String title, List<Movie> movies) {
         List<Movie> result = new ArrayList<>();
         for (Movie movie: movies) {
             if (movie.getTitle().contains(title)) result.add(movie);
