159c42949d314d116c165d6c3c888e811a66914d
Jean-Baptise WATENBERG <jeanbaptiste.watenberg@gmail.com>
2018-04-21 12:15:05 +0200

    add readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Java 8 functional programing attempt
+
+Based on https://github.com/FaustXVI/functional-programming-introduction
+ talk and source, this project is an attempt to reproduce the talk with Java 8.
+ 
--- src/main/java/com/jb/fp/JavaFp.java
+++ src/main/java/com/jb/fp/JavaFp.java
@@ -10,11 +10,20 @@ import java.util.stream.Collectors;
  */
 public class JavaFp {
 
-    private Function<String, Function<String, Boolean>> isInfixOf = title -> whole -> whole.contains(title);
-    private Function<Function<Movie, Boolean>, Function<Collection<Movie>, Collection<Movie>>> filter = predicate -> collection -> collection.stream().filter(movie -> predicate.apply(movie)).collect(Collectors.toList());
-    private Function<Movie, String> getTitle = Movie::getTitle;
-    private Function<String, Function<Movie, Boolean>> matches = title -> movie -> isInfixOf.apply(title).apply(getTitle.apply(movie));
-    public Function<String, Function<Collection<Movie>, Collection<Movie>>> findByTitle = query -> movies -> filter.apply(matches.apply(query)).apply(movies);
+    private Function<String, Function<String, Boolean>>
+        isInfixOf = stringQuery -> whole -> whole.contains(stringQuery);
+
+    private Function<Function<Movie, Boolean>, Function<Collection<Movie>, Collection<Movie>>>
+        filter = predicate -> collection -> collection.stream().filter(predicate::apply).collect(Collectors.toList());
+
+    private Function<Movie, String>
+        getTitle = Movie::getTitle;
+
+    private Function<String, Function<Movie, Boolean>>
+        matches = title -> movie -> isInfixOf.apply(title).apply(getTitle.apply(movie));
+
+    public Function<String, Function<Collection<Movie>, Collection<Movie>>>
+        findByTitle = query -> movies -> filter.apply(matches.apply(query)).apply(movies);
 
     public List<Movie> findByTitleBasic(String title, List<Movie> movies) {
         List<Movie> result = new ArrayList<>();
